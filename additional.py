ccf1a68d2520c9b6bfe21cb80d1ef3d236258006
Sri Ranga Sai Pagala <53367382+theunhackable@users.noreply.github.com>
2021-11-30 13:46:15 +0530

    updated to kowshik's code

--- additional.py
+++ additional.py
@@ -7,46 +7,24 @@ both should accept a string and and return a string,  either encrypted or decryp
 
 """
 
+sp="""abcdefghijklmnopqrstuv!"#$%&'()*+,-. MNOPQRSTUVWXYZ/:;<=>?@[\]^_`{|}~0123456789wxyzABCDEFGHIJKL"""
 
-def contents(data):
-    file_content = []
-    for dat in data:
-        file_content.append(dat) 
-    return file_content
+def encrypt(pat):
+    newpat=""
+    for i in range(len(pat)):
+        if(pat[i] in sp):
+            newpat += sp[(sp.index(pat[i]) + i) % len(sp)]
+    return newpat
 
-#rot 13
-
-alpha_numeric = "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-symbols = " ~`!@#$%^&*()_+-={}[]|\\:;\"'<>,.?/"
-rot = 13
-key = alpha_numeric + symbols
-key_len = len(key)
-
-def encrypt(data):
-    
-    encrypted = ""
-    for letter in data:
-        for pos in range(key_len):
-            if key[pos] == letter:
-                new_pos = pos + rot
-                if new_pos < key_len:
-                    encrypted += key[new_pos]
-                else:
-                    encrypted += key[new_pos - key_len]
-    return encrypted
             
+def decrypt(pat):
+    newpat=""
+    for i in range(len(pat)):
+        if(pat[i] in sp):
+            newpat += sp[(sp.index(pat[i]) - i) % len(sp)] 
+    return newpat
 
-def decrypt(data):
-    decrypted = ""
-    for letter in data:
-        for pos in range(key_len):
-            if key[pos] == letter:
-                new_pos = pos - rot
-                if new_pos >= 0:
-                    decrypted += key[new_pos]
-                else:
-                    decrypted += key[key_len + new_pos]
-    return decrypted
+
 
 def to_utf8(data):
     contents = []
@@ -69,8 +47,3 @@ def decrypted_text(data):
     for i in data:
         decrypted.append(decrypt(i) + '\n')
     return decrypted
-
-
-a = ['hello\n', 'mowa\n']
-with open('temp.txt', 'w') as temp_file:
-    temp_file.writelines(encrypted_text(a))
